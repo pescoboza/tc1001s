ead0053b0c31c582d7e58ba5e3b2e700e6217eda
Quevedo <jlqo21@gmail.com>
2020-10-27 18:28:12 -0700

    :)

--- games/shapes1.py
+++ games/shapes1.py
@@ -19,3 +19,28 @@ while True:
     elif (answer == '2'):
         filledSquare()
  
+import turtle
+from turtle import *
+from turtle import Screen
+from freegames import vector
+
+def emptyCircle():
+    pass  # TODO
+
+def filledCircle():
+    pass  # TODO
+
+def triangle():
+    pass  # TODO
+
+while True:
+    screen = Screen()
+    answer = screen.textinput("Next Game","1 - Square:")
+    if (answer is None):
+        break
+    elif (answer == '3'):
+        emptyCircle()
+    elif (answer == '4'):
+        filledCircle()
+    elif (answer == '5'):
+        triangle()
